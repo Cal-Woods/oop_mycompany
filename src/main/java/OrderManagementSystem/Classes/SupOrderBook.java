5781c3558179e9f0799dac412c7c7ec19ffb9e3f
Cal Woods <D00270955@student.dkit.ie>
2024-11-26 23:07:02 +0000

    Updated SupOrderBook class.

--- src/main/java/OrderManagementSystem/Classes/SupOrderBook.java
+++ src/main/java/OrderManagementSystem/Classes/SupOrderBook.java
@@ -1,5 +1,30 @@
 package OrderManagementSystem.Classes;
 
+import java.util.ArrayList;
+
 public class SupOrderBook {
-    
+    //Declare attributes
+    private ArrayList<SupOrder> orders = new ArrayList<SupOrder>(0);
+
+    //Getter methods
+    public ArrayList<SupOrder> getOrders() {
+        return orders;
+    }
+
+    //Mutator methods
+    public void add(SupOrder order) {
+        //Validation
+        if(order == null)
+            throw new IllegalArgumentException("Given SupOrder array must NOT be null.");
+
+        //Use ArrayList add() method to add order to orders
+        this.orders.add(order);
+    }
+    public void add(ArrayList<SupOrder> orders) {
+        //Validation
+        if(orders == null)
+            throw new IllegalArgumentException("Given ArrayList must NOT be null.");
+        
+        this.orders.addAll(orders);
+    }
 }
