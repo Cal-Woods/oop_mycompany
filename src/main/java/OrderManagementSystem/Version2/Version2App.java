2e9ab484b6c7b2fc31dd0230e55233a8459d5b36
Cal Woods <D00270955@student.dkit.ie>
2024-11-27 23:37:41 +0000

    Created Version2App class.

---
+++ src/main/java/OrderManagementSystem/Version2/Version2App.java
@@ -0,0 +1,17 @@
+package OrderManagementSystem.Version2;
+
+import java.util.Scanner;
+
+import OrderManagementSystem.Classes.*;
+
+public class Version2App {
+    public static void main(String[] args) {
+    //Add Scanner object
+    Scanner sc = new Scanner(System.in);
+
+    //Declare Stock() object
+    Stock stock = new Stock();
+
+    //Declare ArrayList   
+    }
+}
