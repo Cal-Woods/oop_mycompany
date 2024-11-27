1389b0532f6bf18a55472781d7b96e26b556c9fb
Cal Woods <D00270955@student.dkit.ie>
2024-11-27 23:15:11 +0000

    Updated Stock class to include findItem( & findDearest( methods.

---
+++ src/main/java/OrderManagementSystem/Classes/Stock.java
@@ -0,0 +1,68 @@
+package OrderManagementSystem.Classes;
+
+import java.util.ArrayList;
+
+public class Stock {
+    //Attribute: ArrayList<Item> products
+    private ArrayList<Item> products = new ArrayList<Item>(0);
+
+    //No argument constructor
+    public Stock() {
+        //Create blank ArrayList<Item>
+        products = new ArrayList<Item>(0);
+    }
+
+    //Getters
+    public ArrayList<Item> getProducts() {
+        return products;
+    }
+
+    //Setters
+    public boolean add(Item item) {
+        //Validation
+        if(item == null)
+            throw new IllegalArgumentException("Given ArrayList<Item> must NOT be null.");
+
+        //Use ArrayList<T> addAll method to add given items to instance ArrayList<Item> products
+        products.add(item);
+        return true;
+    }
+
+    //Mutator methods
+    public Item findItem(String itemName) {
+        //Validation
+        if(itemName == null)
+            throw new IllegalArgumentException("Given Item object must NOT be null.");
+        if(itemName.isEmpty() || this.getProducts().isEmpty()) {
+            return new Item();
+        }
+
+        //Initialise for loop to search ArrayList<Item> products instance variable for given itemName
+        for (int i = 0; i < getProducts().size(); i++) {
+            //Check if Item 
+            if(getProducts().get(i).getName().equals(itemName)) {
+                return getProducts().get(i);
+            }
+        }
+
+        return new Item();
+    }
+
+    public String findDearest(Item item) {
+        //Declare variables
+        double dearest = 0.00;
+        String name = "";
+
+        //Initialise for loop to find dearest Item object in this.products ArrayList<Item>
+        for (int i = 0; i < this.getProducts().size()-1; i++) {
+            //Check if current element is larger dearest
+            if(getProducts().get(i).getUnitCostPrice() > dearest) {
+                dearest = getProducts().get(i).getUnitCostPrice();
+                name = getProducts().get(i).getName();
+            }
+        }
+
+
+        return "";
+    }
+}
