b60aefb2201039a59b87dc678c5eb97776443284
Cal Woods <D00270955@student.dkit.ie>
2025-02-09 17:04:34 +0000

    Added Version3App.java, a java class with 'main()' method.

---
+++ src/main/java/OrderManagementSystem/Version3/Version3App.java
@@ -0,0 +1,9 @@
+package OrderManagementSystem.Version3;
+
+import OrderManagementSystem.Classes.SupplierCollection;
+
+public class Version3App {
+    public static void main(String[] args) {
+        SupplierCollection collection = new SupplierCollection("Binary-File");
+    }
+}
