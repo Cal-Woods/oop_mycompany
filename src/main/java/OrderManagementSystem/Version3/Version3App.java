bd34dfe90e17e17f959b9378443ea0f817764f38
Cal Woods <D00270955@student.dkit.ie>
2025-02-10 10:32:00 +0000

    Updated Version3App class.

--- src/main/java/OrderManagementSystem/Version3/Version3App.java
+++ src/main/java/OrderManagementSystem/Version3/Version3App.java
@@ -1,9 +1,14 @@
 package OrderManagementSystem.Version3;
 
+import OrderManagementSystem.Classes.Supplier;
 import OrderManagementSystem.Classes.SupplierCollection;
 
 public class Version3App {
     public static void main(String[] args) {
         SupplierCollection collection = new SupplierCollection("Binary-File");
+
+        collection.add(new Supplier("Kaitling Phillips", "666 Hells Highway", 564.38, 6000.87));
+
+        System.out.println(collection);
     }
 }
