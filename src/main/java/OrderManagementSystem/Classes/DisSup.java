e6ddc964b198d2bb7ffeb2107839a53db7572269
Cal Woods <D00270955@student.dkit.ie>
2025-03-05 13:36:58 +0000

    Update: Added a no-argument constructor for DisSup class.
    Calls the no-argument constructor for parent class Supplier.
    Initialises DisSup attributes.

--- src/main/java/OrderManagementSystem/Classes/DisSup.java
+++ src/main/java/OrderManagementSystem/Classes/DisSup.java
@@ -7,4 +7,17 @@ public class DisSup extends Supplier {
     //Attributes
     double orderValueForDiscount;
     double discountPercentage;
+
+    //Constructors
+    /**
+     * A no-argument constructor that sets attributes to default values.
+     */
+    public DisSup() {
+        //Call Supplier super constructor
+        super();
+
+        //Initialise this object's attributes
+        this.orderValueForDiscount = 0.00;
+        this.discountPercentage = 0.00;
+    }
 }
