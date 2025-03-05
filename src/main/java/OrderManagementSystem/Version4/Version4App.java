3f7fe4f431be572b50d21bc28a17bf1153439762
Cal Woods <D00270955@student.dkit.ie>
2025-03-05 13:42:57 +0000

    Import: Added necessary import for all classes.
    Added import for the Classes package in project.
    
    Allows Version4App class to make use of project Classes.

--- src/main/java/OrderManagementSystem/Version4/Version4App.java
+++ src/main/java/OrderManagementSystem/Version4/Version4App.java
@@ -1,5 +1,8 @@
 package OrderManagementSystem.Version4;
 
+//Required for Version4 as contains all classes
+import OrderManagementSystem.Classes.*;
+
 public class Version4App {
     public static void main(String[] args) {
         
