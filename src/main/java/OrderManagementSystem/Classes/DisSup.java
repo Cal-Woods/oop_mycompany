bf43b4512aa870cf15d74df61191f80364d01a0d
Cal Woods <D00270955@student.dkit.ie>
2025-03-05 13:16:49 +0000

    Javadocs: Added a javadoc description to DisSup class.

--- src/main/java/OrderManagementSystem/Classes/DisSup.java
+++ src/main/java/OrderManagementSystem/Classes/DisSup.java
@@ -1,5 +1,8 @@
 package OrderManagementSystem.Classes;
 
+/**
+ * Represents a discount supplier which is extended from Supplier class.
+ */
 public class DisSup extends Supplier {
     //Attributes
     double orderValueForDiscount;
