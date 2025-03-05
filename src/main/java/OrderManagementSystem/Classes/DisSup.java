ffb806c779a35b55e4ad619921b42dc749718ab7
Cal Woods <D00270955@student.dkit.ie>
2025-03-05 18:51:12 +0000

    Update: Added constructor that takes arguments for all parent Supplier & child DisSup attributes.
    All attributes of Supplier were initialised with the super() constructor and the child attributes DisSup were initialised.
    
    super() is the constructor of the direct parent class Supplier in this case.

--- src/main/java/OrderManagementSystem/Classes/DisSup.java
+++ src/main/java/OrderManagementSystem/Classes/DisSup.java
@@ -20,4 +20,24 @@ public class DisSup extends Supplier {
         this.orderValueForDiscount = 0.00;
         this.discountPercentage = 0.00;
     }
+
+    /**
+     * Initialises DisSup instance with arguments for costBeforeDiscount & discountPercentage, as well as arguments
+     * for initialising all parent Supplier attributes.
+     * 
+     * @param name Given Supplier name
+     * @param address Given Supplier address
+     * @param amountOwed Given amount owed to Supplier
+     * @param creditLimit Given credit limit
+     * @param orderValueForDiscount Given cost of order before discount is applied, for discount supplier
+     * @param discountPercentage Given discount percentage
+     */
+    public DisSup(String name, String address, double amountOwed, double creditLimit, double orderValueForDiscount, double discountPercentage) {
+        //Call super() with given Supplier parameters
+        super(name, address, amountOwed, creditLimit);
+
+        //Initialise DisSup attributes
+        this.orderValueForDiscount = orderValueForDiscount;
+        this.discountPercentage = discountPercentage;
+    }
 }
