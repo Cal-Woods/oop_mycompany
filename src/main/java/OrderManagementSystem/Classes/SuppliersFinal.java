2fc9936d26bb8a11a9bbc74e1a244a730eddc14c
Cal Woods <D00270955@student.dkit.ie>
2025-03-12 19:27:44 +0000

    Update: Added displayAllName() method.
    Displays all Supplier objects in SuppliersFinal by name ascending.

--- src/main/java/OrderManagementSystem/Classes/SuppliersFinal.java
+++ src/main/java/OrderManagementSystem/Classes/SuppliersFinal.java
@@ -1,5 +1,6 @@
 package OrderManagementSystem.Classes;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class SuppliersFinal {
@@ -23,7 +24,25 @@ public class SuppliersFinal {
     public boolean add(Supplier supplier) {
         //Validation
         if(supplier == null) throw new IllegalArgumentException("Given supplier was null. This is NOT a valid argument.");
-        
+
         return this.sup.add(supplier);
+    }
+
+    /**
+     * Prints all Supplier objects in instance.
+     * 
+     * @return True if printing successful, false otherwise.
+     */
+    public boolean displayAllName() {
+        //Validation
+        if(this.sup.isEmpty()) return false;
+
+        ArrayList<Supplier> temp = new ArrayList<Supplier>(this.sup);
+
+        temp.sort((s1, s2) -> {return 1 * s1.getName().compareToIgnoreCase(s2.getName());});
+
+        temp.forEach((supplier) -> {System.out.println(supplier+"\n");});
+
+        return true;
     }
 }
