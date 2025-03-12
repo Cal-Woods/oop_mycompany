becfc3a3044b38ac8d87a89804bd9e46a115f2f2
Cal Woods <D00270955@student.dkit.ie>
2025-03-12 18:38:44 +0000

    Update: Added a no-argument constructor.
    Constructor initialises a blank TreeSet attribute.

--- src/main/java/OrderManagementSystem/Classes/SuppliersFinal.java
+++ src/main/java/OrderManagementSystem/Classes/SuppliersFinal.java
@@ -1,5 +1,15 @@
 package OrderManagementSystem.Classes;
 
+import java.util.TreeSet;
+
 public class SuppliersFinal {
-    
+    private TreeSet<Supplier> sup;
+
+    //Constructors
+    /**
+     * Initialises blank SuppliersFinal object.
+     */
+    public SuppliersFinal() {
+        sup = new TreeSet<Supplier>();
+    }
 }
